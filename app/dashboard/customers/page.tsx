95481296b6424ebc19aaf3241b653795a27716b2
Manoj.kumar <mannu9058@gmail.com>
2024-01-15 18:12:28 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+const page= ()=>{
+    return <>
+     <p>
+        customers page
+     </p>
+    </>
+}
+
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+const page= ()=>{
+    return <>
+     <p>
+        invoices page
+     </p>
+    </>
+}
+
+export default page;
